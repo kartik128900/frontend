4ddea60fffbbe1078aa4808e085fc6d1e9703df1
Kartik Jain <104304076+kartik128900@users.noreply.github.com>
2024-12-07 13:24:25 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
